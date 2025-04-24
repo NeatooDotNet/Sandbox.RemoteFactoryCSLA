e61fd9534451651e74dadf305b6b4ca00f54e534
KeithVoels <keithvoels@gmail.com>
2025-04-24 14:45:10 -0500

    Combined everything

--- RemoteFactoryCSLA.Tests/CSLABusinessBaseTests.cs
+++ RemoteFactoryCSLA.Tests/CSLABusinessBaseTests.cs
@@ -3,43 +3,37 @@ using Csla;
 using Csla.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
+using System;
 
 namespace RemoteFactoryCSLA.Tests
 {
     public class CSLABusinessBaseTests
     {
-        private static IServiceProvider serviceProvider;
-        public CSLABusinessBaseTests()
-        {
-            if (serviceProvider == null)
-            {
-                var serviceCollection = new ServiceCollection();
-                serviceCollection.AddCsla(o =>
-                            o.DataPortal(o => 
-                                o.AddServerSideDataPortal(o => o.RemoveInterceptorProvider(0))));
-                // If I include this then it doesn't call the Create method even the first time
-                //serviceCollection.AddTransient<IDataPortal<CSLABusinessBase>>(cc => Mock.Of<IDataPortal<CSLABusinessBase>>());
-                serviceProvider = serviceCollection.BuildServiceProvider();
-            }
-        }
+
 
         [Fact]
         public void CSLABusinessBase_Create()
         {
-            var factory = serviceProvider.GetRequiredService<IDataPortalFactory>();
-            var cslaBBFactory = factory.GetPortal<CSLABusinessBase>();
+            var mockDP = new Mock<IDataPortal<CSLABusinessBase>>(MockBehavior.Strict);
 
-            // var mockDP = new Mock<IDataPortal<CSLABusinessBase>>(MockBehavior.Strict);
-            // mockDP.Setup(x => x.Create(2)).Returns(Mock.Of<CSLABusinessBase>());
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddCsla(o =>
+                        o.DataPortal(o =>
+                            o.AddServerSideDataPortal(o => o.RemoveInterceptorProvider(0))));
 
-            // Right now this returns all 32k objects which is not ok
-            // How do I moq IDataPortal<CSLABusinessBase>???
+            serviceCollection.AddSingleton(cc => mockDP.Object);
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            
+            var cslaBBFactory = serviceProvider.GetService<IDataPortal<CSLABusinessBase>>();
+
+            mockDP.Setup(x => x.Create(2)).Returns(Mock.Of<CSLABusinessBase>());
+
             var cslaBB = cslaBBFactory.Create();
-
 
             Assert.Equal(1, cslaBB.Id);
             Assert.NotNull(cslaBB.Description);
-            // mockDP.Verify(x => x.Create(2), Times.Exactly(2));
+            mockDP.Verify(x => x.Create(2), Times.Exactly(2));
         }
     }
 }
